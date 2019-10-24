5c2f4b8dc7beefa157cf68489f41fe84c8319bcf
copperTones <evilcreeper64@gmail.com>
2019-10-24 11:59:13 -0500

    image means added to dicts
    sorted based on solid vs. transparent

--- megablocks.py
+++ megablocks.py
@@ -4,8 +4,8 @@ from PIL import ImageStat
 
 def printRGB(rgb):
 	for i, c in enumerate(rgb):
-		print(f'{c:.0f}', 'RGB'[i], sep='', end=' ')
-	print('\n')
+		print(f'{c:.0f}', 'RGBA'[i], sep='', end=' ')
+	print('')
 
 
 srcPack = 'assets'
@@ -16,14 +16,25 @@ dir = os.path.join(os.getcwd(), srcPack, 'minecraft', 'textures', 'block')
 for path, folders, files in os.walk(dir):#add all blocks to imgList
 	for file in files:
 		imgList.append(os.path.join(path, file))
-print(imgList)
 
 print('Sorting files')
+pixelSubs = {}#images that can replace solid pixels
+pixelSubsA = {}#images that can replace transparent pixels
 for file in imgList:
 	try:
 		img = Image.open(file)
+		img.convert("RGBA")
 		st = ImageStat.Stat(img)
 		printRGB(st.mean)
+		if st.mean[3] == 255:#all solid
+			r, g, b, a = st.mean
+			pixelSubs[(r, g, b)] = file
+		else:#any transparency
+			pixelSubs[st.mean] = file
 	except:
 		pass
+input()
+print(pixelSubs)
+input()
+print(pixelSubsA)
 input()
